2fd0477c4406afe838be57b5f8c50546dd8e7458
Surya Nikhil Mallampalli <suryanikhil.m@gmail.com>
2023-01-08 11:50:02 +0530

    Add files via upload
    This is a TodoList Project [Server Oriented]. This is the first version I have developed.
    Using the following languages
    HTML, CSS, JavaScript, Node JS, Embedded Javascript Templating & Layout.
    Note: In order to execute the described code, one has to import the node modules as well.

---
+++ date.js
@@ -0,0 +1,13 @@
+//jshint esversion:6
+console.log(module);
+function getDate(){
+    let today=new Date();
+    let options={
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+    };
+    
+    let day=today.toLocaleDateString("en-US",options);
+    return day;
+}
